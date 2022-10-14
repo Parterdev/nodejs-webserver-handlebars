eccfd354062be46536a6a6b1d9342d6dc367bb54
Parterdev <litemeilateran@outlook.com>
2022-10-14 15:58:57 -0500

    Add and change app-angular.js file to render angular app from node.js

---
+++ app-angular.js
@@ -0,0 +1,22 @@
+const express = require('express'); // It's a function
+const app = express(); 
+const hbs = require('hbs');
+// Upload env variables
+require('dotenv').config();
+const port = process.env.PORT;
+
+// Here: Without engine
+
+// Serve static content
+app.use(express.static('public/angular-app'));
+
+// Tricky route to catch 404 not found pages
+app.get('*', (req, res) => {
+  // Before send string param => res.send('404 | Not found');
+  res.sendFile(__dirname + '/public/404.html');
+}); 
+
+
+app.listen(port, () => {
+  console.log(`My express app listening on ${port}`);
+});
